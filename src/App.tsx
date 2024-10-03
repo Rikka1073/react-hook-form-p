6ab0452bd97f8e8ad12ae7e86f6159ec19eb4c5f
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-10-03 23:38:55 +0900

    バリデーションの設定

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,11 @@ type FormValues = {
 };
 
 function App() {
-  const { register, handleSubmit } = useForm<FormValues>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ mode: "onBlur" });
   const onsubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
   };
@@ -17,7 +21,7 @@ function App() {
       <form onSubmit={handleSubmit(onsubmit)} className={css({ textAlign: "left", background: "gray.50", lg: { width: "1/4", margin: "0 auto" }, margin: "6", padding: "4" })}>
         <p className={css({ textAlign: "center", fontSize: "2xl", fontWeight: "bold" })}>Form</p>
         <ul>
-          <li className={css({ marginTop: "5" })}>
+          <li className={css({ marginTop: "5", fontWeight: "bold" })}>
             <label className={css({ marginRight: "5" })}>名前</label>
             <input
               placeholder="名前"
@@ -31,8 +35,13 @@ function App() {
               })}
               {...register("name", { required: "名前を入力してください" })}
             />
+            {errors.name && (
+              <p role="alert" className={css({ color: "red.500", fontWeight: "bold" })}>
+                {errors.name.message}
+              </p>
+            )}
           </li>
-          <li className={css({ marginTop: "5" })}>
+          <li className={css({ marginTop: "5", fontWeight: "bold" })}>
             <label className={css({ marginRight: "5" })}>メール</label>
             <input
               type="text"
@@ -46,9 +55,14 @@ function App() {
               })}
               {...register("mail", { required: "メールアドレスを入力してください" })}
             />
+            {errors.mail && (
+              <p role="alert" className={css({ color: "red.500", fontWeight: "bold" })}>
+                {errors.mail.message}
+              </p>
+            )}
           </li>
           <li className={css({ marginTop: "5" })}>
-            <label className={css({ marginRight: "5" })}>パスワード</label>
+            <label className={css({ marginRight: "5", fontWeight: "bold" })}>パスワード</label>
             <input
               type="text"
               placeholder="パスワード"
@@ -61,9 +75,16 @@ function App() {
               })}
               {...register("password", { required: "パスワードを入力してください" })}
             />
+            {errors.password && (
+              <p role="alert" className={css({ color: "red.500", fontWeight: "bold" })}>
+                {errors.password.message}
+              </p>
+            )}
           </li>
         </ul>
-        <button type="submit">送信</button>
+        <button type="submit" className={css({ background: "blue.400", color: "white", p: "4", width: "1/2", margin: "0 auto", display: "block", mt: "5", fontWeight: "bold" })}>
+          送信
+        </button>
       </form>
     </>
   );
